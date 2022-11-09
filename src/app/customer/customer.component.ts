e8734330e1bf350dc690bec38f7bca86d24ba7ca
Jesper Holm <jesperliholm@gmail.com>
2022-11-09 14:32:59 +0100

    woops

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CustomerComponent implements OnInit {
 
   name = '';
-  customers:[] = [];
+  customers:string[] = [];
   
   constructor() {
 
