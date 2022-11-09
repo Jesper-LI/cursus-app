78238b4c4a31080bc328f86d4b74f0a9eff9a71a
Jesper Holm <jesperliholm@gmail.com>
2022-11-09 16:15:09 +0100

    Added formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,6 +18,7 @@ import { ColourpatchComponent } from './colourpatch/colourpatch.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     NgbModule
   ],
