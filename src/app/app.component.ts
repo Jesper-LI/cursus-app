888c03e093e7e45dba04b3d219bb15c1e777cf10
Jesper Holm <jesperliholm@gmail.com>
2022-11-10 15:31:49 +0100

    Day2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { City } from "./city";
 import { CityService } from './services/city.service';
 
@@ -10,21 +11,23 @@ import { CityService } from './services/city.service';
 export class AppComponent {
   title = 'Welkom dit is een angular Cursus';
   name = 'Henkus Dingus';
-  cities:City[] = [];
-  showCities:Boolean = true;
+  cities: City[] = [];
+  cities$!:Observable<City[]>;
+  showCities: Boolean = true;
   toggleMsg = 'Verberg lijst met steden';
-  nlText= {
-    t1 : 'Verberg lijst met steden',
-    t2 :'Toon lijst met steden LNG'
+  nlText = {
+    t1: 'Verberg lijst met steden',
+    t2: 'Toon lijst met steden LNG'
   }
-  currentCity: City = new City(0,'','');
+  currentCity: City = new City(0, '', '');
 
-  constructor(private cityService : CityService) {
+  constructor(private cityService: CityService) {
 
   }
 
   ngOnInit(): void {
-    this.cities = this.cityService.getCities();
+    // this.cityService.getCities().subscribe(cityArray => this.cities = cityArray);
+    this.cities$ = this.cityService.getCities();
   }
 
   toggleCities(event: Event) {
@@ -37,11 +40,11 @@ export class AppComponent {
     this.currentCity = city;
     // alert(`Uw favoriete stad is ${this.currentCity.name}`);
   }
-  addCity(cityName: string, cityPhotoUrl:string, provinceIndex:number) {
+  addCity(cityName: string, cityPhotoUrl: string, provinceIndex: number) {
     this.cityService.addCity(cityName, cityPhotoUrl, provinceIndex);
   }
 
-  getProvinces(){
+  getProvinces() {
     return this.cityService.getProvinces();
   }
 }
--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { City } from '../city';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
-
+  
   private cities: City[] = [
-    new City(1, 'Groningen', 'GR', "https://www.27vakantiedagen.nl/wp-content/uploads/2021/02/groningen-stad.jpg"),
-    new City(2, 'Hengelo', 'OV', "https://hengelo.nu/wp-content/uploads/2021/09/Enschedesestraat-Hengelo-4.jpg"),
-    new City(3, 'Den Haag', 'ZH', "https://www.reisroutes.be/userfiles/fotos/skyline-van-den-haag-nederland_28879_xl.jpg"),
-    new City(4, 'Assen', 'DR', "https://www.holland.com/upload_mm/2/0/c/78014_fullimage_people%20enjoying%20the%20spring%20weather%20at%20restaurants%20in%20assen%20%C2%A9%20marc%20venema%20via%20shutterstock.jpg"),
   ];
 
   private province = {
@@ -28,10 +27,13 @@ export class CityService {
     12: 'FR'
   };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getCities() {
-    return this.cities
+  getCities(): Observable<City[]> {
+    return this.http
+      .get<City[]>('../assets/cities.json')
+      .pipe(tap(result => console.log(`Dit komt binnen als Observable : ${result}`)
+      ))
   }
 
   getCity(id: number) {
